452ce950303babb126f16e8ffd251691c9d6fdf4
muti1967 <mutishuman@gmail.com>
2023-09-08 20:37:19 -0700

    big changes to multiple files
    big changes to multiple files

---
+++ script.js
@@ -0,0 +1,62 @@
+// Form submission
+const contactForm = document.getElementById('contact-form');
+contactForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the form from submitting wihtout info
+
+    // Fetch form data
+    const formData = new FormData(contactForm);
+
+//will complete API when i have a chance next project
+
+
+    fetch('your-server-endpoint', {
+        method: 'POST',
+        body: formData,
+    })
+        .then(response => response.json())
+        .then(data => {
+            //show a success message
+            console.log(data);
+        })
+        .catch(error => {
+            // show an error message
+            console.error(error);
+        });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll("nav a");
+
+    navLinks.forEach(function (link) {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute("href").substring(1); 
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                const offsetTop = targetSection.getBoundingClientRect().top + window.scrollY;
+                window.scroll({
+                    top: offsetTop,
+                    behavior: "smooth", 
+                });
+            }
+        });
+    });
+});
+const messageSentDiv = document.getElementById('message-sent');
+
+function handleSubmit(event) {
+    event.preventDefault(); 
+    showMessageSent();
+}
+
+function showMessageSent() {
+    messageSentDiv.style.display = 'block'; 
+    setTimeout(() => {
+        messageSentDiv.style.display = 'none'; 
+    }, 3000); 
+}
+
+// Event listener for form submission
+contactForm.addEventListener('submit', handleSubmit);
