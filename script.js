7e7da62dc3f64e6e09d029d2a5672142f96d67cc
muti1967 <mutishuman@gmail.com>
2023-09-12 14:51:39 -0700

    fixed background.. next fix upper bounds

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
+// Form submission
+const contactForm = document.getElementById('contact-form');
+contactForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the form from submitting wihtout info
+
+    // Fetch form data
+    const formData = new FormData(contactForm);
+
+//will complete API when i have a chance next project
+
+
+    fetch('your-server-endpoint', {
+        method: 'POST',
+        body: formData,
+    })
+        .then(response => response.json())
+        .then(data => {
+            //show a success message
+            console.log(data);
+        })
+        .catch(error => {
+            // show an error message
+            console.error(error);
+        });
+});
+
+//cool effects section
+
+const parallax = document.querySelector('.parallax');
+let scrollValue = 0;
+
+function moveBackgroundVertical() {
+    scrollValue += 0.1; // Adjust the increment to control the speed
+    parallax.style.backgroundPositionY = `${scrollValue}px`;
+
+    // Reset the position when it reaches the end
+    if (scrollValue >= window.innerHeight) {
+        scrollValue = 0;
+    }
+    requestAnimationFrame(moveBackgroundVertical);
+}
+
+moveBackgroundVertical(); // Start the animation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const navLinks = document.querySelectorAll("nav a");
 
@@ -5,49 +65,32 @@ document.addEventListener("DOMContentLoaded", function () {
         link.addEventListener("click", function (e) {
             e.preventDefault();
 
-            const targetId = this.getAttribute("href").substring(1);
+            const targetId = this.getAttribute("href").substring(1); 
             const targetSection = document.getElementById(targetId);
 
             if (targetSection) {
                 const offsetTop = targetSection.getBoundingClientRect().top + window.scrollY;
                 window.scroll({
                     top: offsetTop,
-                    behavior: "smooth",
+                    behavior: "smooth", 
                 });
             }
         });
     });
-
-    const contactForm = document.getElementById('contact-form');
-    contactForm.addEventListener('submit', function (event) {
-        event.preventDefault(); // Prevent the form from submitting wihtout info
-
-        // Fetch form data
-        const formData = new FormData(contactForm);
-
-        // Replace 'your-server-endpoint' with your actual server endpoint
-        fetch('your-server-endpoint', {
-            method: 'POST',
-            body: formData,
-        })
-            .then(response => response.json())
-            .then(data => {
-                // Show a success message
-                console.log(data);
-                showMessageSent();
-            })
-            .catch(error => {
-                // Show an error message
-                console.error(error);
-            });
-    });
-
-    const messageSentDiv = document.getElementById('message-sent');
-
-    function showMessageSent() {
-        messageSentDiv.style.display = 'block';
-        setTimeout(() => {
-            messageSentDiv.style.display = 'none';
-        }, 3000);
-    }
 });
+const messageSentDiv = document.getElementById('message-sent');
+
+function handleSubmit(event) {
+    event.preventDefault(); 
+    showMessageSent();
+}
+
+function showMessageSent() {
+    messageSentDiv.style.display = 'block'; 
+    setTimeout(() => {
+        messageSentDiv.style.display = 'none'; 
+    }, 3000); 
+}
+
+// Event listener for form submission
+contactForm.addEventListener('submit', handleSubmit);
